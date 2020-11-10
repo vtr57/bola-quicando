73e3e854ac7b0ac418f3ec401306e630d3a077a0
vtr57 <professorvitorfisica@gmail.com>
2020-11-10 17:46:59 -0300

    Inserindo teste e atualizando main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import quedalivre
 
-corpo = quedalivre.Particula(v0=100, g=10, k_p=0.1)
+corpo = quedalivre.Particula(v0=25, g=10, k_p=0.15, n=50)
 corpo.tempo()
 corpo.velocidade()
 corpo.posicao()
